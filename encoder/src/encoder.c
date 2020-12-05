ff8018608d885ae5175f19c3c51ebdcb7c431a20
Colin Smith <davidcolinsmith@gmail.com>
2020-12-04 16:25:53 -0800

    adding encoder project

---
+++ encoder/src/encoder.c
@@ -0,0 +1,108 @@
+#include <driver/gpio.h>
+#include <driver/spi_master.h>
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <stdio.h>
+#include <string.h>
+#include "u8g2_esp32_hal.h"
+#include <rotary_encoder.h>
+#include <esp_system.h>
+
+#define ROT_ENC_A_GPIO (5)
+#define ROT_ENC_B_GPIO (18)
+#define ENC_A   5
+#define ENC_B   18
+#define ENC_SW  19
+#define ENABLE_HALF_STEPS false  // Set to true to enable tracking of rotary encoder at half step resolution
+#define RESET_AT          0      // Set to a positive non-zero number to reset the position if this value is exceeded
+#define FLIP_DIRECTION    false  // Set to true to reverse the clockwise/counterclockwise sense
+
+
+#define PIN_SDA 21 
+#define PIN_SCL 22
+
+static const char *TAG = "ssd1306";
+
+void app_main() {
+	u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
+	u8g2_esp32_hal.sda   = PIN_SDA;
+	u8g2_esp32_hal.scl  = PIN_SCL;
+	u8g2_esp32_hal_init(u8g2_esp32_hal);
+
+
+	u8g2_t u8g2; // a structure which will contain all the data for one display
+	u8g2_Setup_ssd1306_i2c_128x32_univision_f(
+		&u8g2,
+		U8G2_R0,
+		//u8x8_byte_sw_i2c,
+		u8g2_esp32_i2c_byte_cb,
+		u8g2_esp32_gpio_and_delay_cb);  // init u8g2 structure
+	u8x8_SetI2CAddress(&u8g2.u8x8,0x78);
+
+	ESP_LOGI(TAG, "u8g2_InitDisplay");
+	u8g2_InitDisplay(&u8g2); // send init sequence to the display, display is in sleep mode after this,
+
+	ESP_LOGI(TAG, "u8g2_SetPowerSave");
+	u8g2_SetPowerSave(&u8g2, 0); // wake up display
+	ESP_LOGI(TAG, "u8g2_ClearBuffer");
+	u8g2_ClearBuffer(&u8g2);
+	ESP_LOGI(TAG, "u8g2_DrawBox");
+	u8g2_DrawBox(&u8g2, 0, 26, 80,6);
+	u8g2_DrawFrame(&u8g2, 0,26,100,6);
+
+	ESP_LOGI(TAG, "u8g2_SetFont");
+        u8g2_SetFont(&u8g2, u8g2_font_ncenB14_tr);
+	ESP_LOGI(TAG, "u8g2_DrawStr");
+        u8g2_DrawStr(&u8g2, 2,17,"Hi, Colin!");
+	ESP_LOGI(TAG, "u8g2_SendBuffer");
+	u8g2_SendBuffer(&u8g2);
+
+	ESP_LOGI(TAG, "All done!");
+
+    // esp32-rotary-encoder requires that the GPIO ISR service is installed before calling rotary_encoder_register()
+    ESP_ERROR_CHECK(gpio_install_isr_service(0));
+
+    // Initialise the rotary encoder device with the GPIOs for A and B signals
+    rotary_encoder_info_t info = { 0 };
+    ESP_ERROR_CHECK(rotary_encoder_init(&info, ROT_ENC_A_GPIO, ROT_ENC_B_GPIO));
+    ESP_ERROR_CHECK(rotary_encoder_enable_half_steps(&info, ENABLE_HALF_STEPS));
+#ifdef FLIP_DIRECTION
+    ESP_ERROR_CHECK(rotary_encoder_flip_direction(&info));
+#endif
+
+    // Create a queue for events from the rotary encoder driver.
+    // Tasks can read from this queue to receive up to date position information.
+    QueueHandle_t event_queue = rotary_encoder_create_queue();
+    ESP_ERROR_CHECK(rotary_encoder_set_queue(&info, event_queue));
+
+    while (1)
+    {
+        // Wait for incoming events on the event queue.
+        rotary_encoder_event_t event = { 0 };
+        if (xQueueReceive(event_queue, &event, 1000 / portTICK_PERIOD_MS) == pdTRUE)
+        {
+            ESP_LOGI(TAG, "Event: position %d, direction %s", event.state.position,
+                     event.state.direction ? (event.state.direction == ROTARY_ENCODER_DIRECTION_CLOCKWISE ? "CW" : "CCW") : "NOT_SET");
+        }
+        else
+        {
+            // Poll current position and direction
+            rotary_encoder_state_t state = { 0 };
+            ESP_ERROR_CHECK(rotary_encoder_get_state(&info, &state));
+            ESP_LOGI(TAG, "Poll: position %d, direction %s", state.position,
+                     state.direction ? (state.direction == ROTARY_ENCODER_DIRECTION_CLOCKWISE ? "CW" : "CCW") : "NOT_SET");
+
+            // Reset the device
+            if (RESET_AT && (state.position >= RESET_AT || state.position <= -RESET_AT))
+            {
+                ESP_LOGI(TAG, "Reset");
+                ESP_ERROR_CHECK(rotary_encoder_reset(&info));
+            }
+        }
+    }
+    ESP_LOGE(TAG, "queue receive failed");
+
+    ESP_ERROR_CHECK(rotary_encoder_uninit(&info));
+
+}
